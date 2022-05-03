ea9d659d1963a98c5604e30c3e5db44c2409bf8d
VitorKaeZ <vitorcaetanoz@outlook.com>
2022-05-03 04:27:51 -0300

    end project

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-Crud simples criado com HTML/CSS,BOOTSTRAP,JAVASCRIPT e salvando os dados no localStorage do navegador.
+Crud simples criado com HTML/CSS,BOOTSTRAP,JAVASCRIPT,JQUERY e salvando os dados no localStorage do navegador.
+
+Materiais usados no estudo:
+
+https://getbootstrap.com
+https://www.w3schools.com
+CRUD Completo com HTML, CSS e JavaScript - Parte 01
+https://www.youtube.com/watch?v=Ihwvv_LvGrc&list=WL&index=4&t=698s
+CRUD Completo com HTML, CSS e JavaScript - Parte 02
+https://www.youtube.com/watch?v=Jny6UH98mPk
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,36 @@
 var dados = []
 
-function PopulaTabela() {
+//Apaga o Registro
+function ApagaRegistro(id){
+    let _confirm = confirm("Deseja realmente excluir este registro?")
+    if (_confirm) {
+        for(let i = 0; i < dados.length; i++) {
+            if (dados[i].Id == id){
+                dados.splice(i, 1)
+            }
+        }
+        AddTabela()
+    }
+}
+
+//Edita o Registro
+function EditaRegistro(id){
+    $("#modalRegistro").modal("show")
+
+    dados.forEach(function(item){
+        if (item.Id == id){
+            $("#hdID").val(item.Id)
+            $("#txtNome").val(item.Nome)
+            $("#txtNascimento").val(item.Nascimento.substr(6, 4) + "-" + item.Nascimento.substr(3, 2) + "-" + item.Nascimento.substr(0, 2))
+            $("#txtDocumento").val(item.Documento)
+        }
+    })
+}
+
+function AddTabela() {
     if (Array.isArray(dados)) {
+
+        localStorage.setItem("__dados__",JSON.stringify(dados))
 
         $("#tblDados tbody").html("")
 
@@ -11,6 +40,8 @@ function PopulaTabela() {
                 <td>${item.Nome}</td>
                 <td>${item.Nascimento}</td>
                 <td>${item.Documento}</td>
+                <td><button type="button" class="btn btn-primary" onclick="javascript:EditaRegistro(${item.Id});"><i class="fa-solid fa-pen-to-square"></i></button></td>
+                <td><button type="button" class="btn btn-danger" onclick="javascript:ApagaRegistro(${item.Id});"><i class="fa-solid fa-trash-can"></i></button></td>
             </tr>`)
         })
     }
@@ -18,11 +49,51 @@ function PopulaTabela() {
 }
 
 $(function() {
-
+//Executa ao carregar a pagina
     dados = JSON.parse(localStorage.getItem("__dados__"))
 
     if (dados) {
-        PopulaTabela()
+        AddTabela()
     }
 
+    $("#btnSalvar").click(function(){
+
+        let _id = $("#hdID").val()
+        let nome = $("#txtNome").val()
+        let nascimento = new Date($("#txtNascimento").val()).toLocaleDateString("pt-br", { timeZone: "UTC"})
+        let documento = $("#txtDocumento").val()
+        
+
+        //Cria ou atualiza um registro
+        if (!_id || _id == "0") {
+            let registro = {}
+            registro.Nome = nome
+            registro.Nascimento = nascimento
+            registro.Documento = documento
+
+            registro.Id = dados.length + 1
+            dados.push(registro)
+        }else{
+            dados.forEach(function(item){
+                if (item.Id == _id) {
+                    item.Nome = nome
+                    item.Nascimento = nascimento
+                    item.Documento = documento
+                }
+            })
+        }
+
+
+        $("#modalRegistro").modal("hide")
+        alert("Registro salvo com sucesso!!")
+
+        
+        //LIMPAR CAMPOS
+        $("#hdID").val("0")
+        $("#txtNome").val("")
+        $("#txtNascimento").val("")
+        $("#txtDocumento").val("")
+
+        AddTabela()
+    })
 })
